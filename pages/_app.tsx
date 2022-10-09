8ddd4d71065eae24eb4dc8d8d479204309db9388
yhf <youssifhany75@gmail.com>
2022-10-10 01:04:49 +0200

    add: chakra ui setup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,37 @@
-import '../styles/globals.css'
+import { ChakraProvider, extendTheme } from '@chakra-ui/react';
+import 'reset-css';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+const theme = extendTheme({
+  colors: {
+    gray: {
+      100: '#F5F5F5',
+      200: '#EEEEEE',
+      300: '#E0E0E0',
+      400: '#BDBDBD',
+      500: '#9E9E9E',
+      600: '#757575',
+      700: '#616161',
+      800: '#424242',
+      900: '#212121',
+    },
+  },
+  components: {
+    Button: {
+      variants: {
+        link: {
+          ':foucs': { outline: 'none', boxShadow: 'none' },
+        },
+      },
+    },
+  },
+});
 
-export default MyApp
+const MyApp = ({ Component, pageProps }) => {
+  return (
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
+};
+
+export default MyApp;
