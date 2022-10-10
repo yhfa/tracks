cb992fd22d975d3bc371682f97bb30d2599168f0
yhf <youssifhany75@gmail.com>
2022-10-10 23:48:22 +0200

    add: layout component

---
+++ components/playerLayout.tsx
@@ -0,0 +1,21 @@
+import { Box } from '@chakra-ui/layout';
+import { FC } from 'react';
+import Sidebar from './sidebar';
+
+const playerLayout: FC = ({ children }) => {
+  return (
+    <Box w="100vw" h="100vh">
+      <Box position="absolute" top="0" left="0" w="250px">
+        <Sidebar />
+      </Box>
+      <Box ml="250px" mb="100px">
+        {children}
+      </Box>
+      <Box position="absolute" left="0" bottom="0">
+        Player
+      </Box>
+    </Box>
+  );
+};
+
+export default playerLayout;
