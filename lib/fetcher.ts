20d54148090d405ecbdc6fd18d7859d94422aa3b
yhf <youssifhany75@gmail.com>
2022-10-15 01:12:55 +0200

    add: types

--- lib/fetcher.ts
+++ lib/fetcher.ts
@@ -1,10 +1,18 @@
-export default function fetcher(url: string, data = undefined) {
-  return fetch(`${window.location.origin}/api${url}`, {
-    method: data ? 'POST' : 'GET',
-    body: JSON.stringify(data),
+export default async function fetcher<T>(
+  url: string,
+  body = undefined
+): Promise<T> {
+  const res = await fetch(`${window.location.origin}/api${url}`, {
+    method: body ? 'POST' : 'GET',
+    body: JSON.stringify(body),
     credentials: 'include',
     headers: {
       'Content-Type': 'application/json',
     },
   });
+
+  const { status, data } = await res.json();
+  if (status === 'error') throw new Error(data.message);
+
+  return data as T;
 }
--- lib/mutations.ts
+++ lib/mutations.ts
@@ -1,8 +1,9 @@
+import { User } from '@prisma/client';
 import fetcher from './fetcher';
 
 export const auth = (
   mode: 'signup' | 'signin',
   body: { email: string; password: string }
 ) => {
-  return fetcher(`/${mode}`, body);
+  return fetcher<User>(`/${mode}`, body);
 };
